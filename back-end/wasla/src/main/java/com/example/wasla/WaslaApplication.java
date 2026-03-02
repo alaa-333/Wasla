24849b8542d999a1ea1afaf4326aebf1458d9a91
Alaa <333alaamo@gmail.com>
2026-03-03 01:18:18 +0200

    first commit

---
+++ back-end/wasla/src/main/java/com/example/wasla/WaslaApplication.java
@@ -0,0 +1,13 @@
+package com.example.wasla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaslaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaslaApplication.class, args);
+	}
+
+}
