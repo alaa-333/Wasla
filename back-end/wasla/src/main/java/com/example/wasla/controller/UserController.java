844bc90134178c9647acd096618d6a8612b8c3dc
Alaa Mohamed <333alaamo@gmail.com>
2026-03-29 15:48:47 +0200

    Delete back-end/wasla directory

--- back-end/wasla/src/main/java/com/example/wasla/controller/UserController.java
+++
@@ -1,4 +0,0 @@
-package com.example.wasla.controller;
-
-public class UserController {
-}
--- back-end/wasla/src/main/java/com/example/wasla/dto/request/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Request DTOs for the Wasla logistics application.
- * <p>
- * This package contains Data Transfer Objects used to receive data from API clients
- * in HTTP request bodies. Request DTOs are mutable to support JSON deserialization
- * and include Jakarta Bean Validation annotations for declarative constraint validation.
- * </p>
- */
-package com.example.wasla.dto.request;
